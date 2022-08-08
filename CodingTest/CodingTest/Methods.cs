32bb8b53a874222eed48f2ebf8f697e0670d17f9
SofiaDrelinskaia <86303496+SofiaDrelinskaia@users.noreply.github.com>
2022-08-08 18:14:55 +0300

    Add files via upload

---
+++ CodingTest/CodingTest/Methods.cs
@@ -0,0 +1,35 @@
+﻿namespace CodingTest
+{
+    internal class Methods
+    {
+        //Problem 1
+        public static List<B> Map<A, B>(List<A> list, Func<A, B> f) {
+            List<B> result = new List<B>();
+            foreach (A x in list)
+            {
+                result.Add(f(x));
+            }
+            return result;
+        }
+
+        //Problem 2
+        public static B Fold<A, B>(List<A> list, B initial, Func<B, A, B> folder) {
+            B result = initial;
+            foreach (A x in list) 
+            {
+                result = folder(result, x);
+            }
+            return result;
+        }
+
+        //Problem 3
+        public static List<B> Map2<A, B>(List<A> list, Func<A, B> f) {
+            List<B> result = new List<B>();
+            foreach (A x in list)
+            {
+                result.Add(Fold<A, B>(new List<A>(new A[] {x}), default, (def, x) => f(x)));
+            }
+            return result;
+        }
+    }
+}
---
+++ CodingTest/CodingTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using CodingTest;
+
+// Problem 1
+
+//Test 1
+List<int> problem1_input1 = new List<int>(new int[] { 1, 2, 3 });
+List<int> problem1_result1 = Methods.Map<int, int>(problem1_input1, x => x + 1);
+
+//Test 2
+List<int> problem1_input2 = new List<int>(new int[] { 1, 2, 3 });
+List<string> problem1_result2 = Methods.Map<int, string>(problem1_input2, x => x.ToString());
+
+//Test 3
+List<bool> problem1_input3 = new List<bool>();
+List<bool> problem1_result3 = Methods.Map<bool, bool>(problem1_input3, null);
+
+
+// Problem 2
+
+//Test 1
+List<int> problem2_input1 = new List<int>(new int[] { 1, 2, 3 });
+int problem2_result1 = Methods.Fold(problem2_input1, 0, (sum, x) => sum + x);
+
+//Test 2
+List<int> problem2_input2 = new List<int>(new int[] { 1, 2, 3 });
+string problem2_result2 = Methods.Fold(problem2_input2, "", (str, x) => str + x.ToString());
+
+//Test 3
+List<int> problem2_input3 = new List<int>();
+int problem2_result3 = Methods.Fold(problem2_input3, 0, null);
+
+
+// Problem 3
+
+//Test 1
+List<int> problem3_input1 = new List<int>(new int[] { 1, 2, 3 });
+List<int> problem3_result1 = Methods.Map2<int, int>(problem3_input1, x => x + 1);
+
+//Test 2
+List<int> problem3_input2 = new List<int>(new int[] { 1, 2, 3 });
+List<string> problem3_result2 = Methods.Map2<int, string>(problem3_input2, x => x.ToString());
+
+//Test 3
+List<bool> problem3_input3 = new List<bool>();
+List<bool> problem3_result3 = Methods.Map2<bool, bool>(problem3_input3, null);
+
+Console.WriteLine("FINISH"); //Установить в этой строке точку останова и посмотреть состояние всех переменных
